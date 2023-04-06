1e2333997f7153dd202c98dc6bf509a446f38af7
sarwar-asik <shasik2021@gmail.com>
2023-04-06 15:19:39 +0600

    errro in useGetQuery

--- src/pages/employer/MyJobs.js
+++ src/pages/employer/MyJobs.js
@@ -3,14 +3,14 @@ import { useGetJobQuery } from "../../features/job/jobApi";
 
 const MyJobs = () => {
   const data = useGetJobQuery();
-  console.log(data, "my jobs");
+
+  console.table("from useGetQuery",data, "my jobs");
   return (
     <div className="grid grid-cols-2  ">
       <section>
         <h2 className="text-3xl font-bold font-serif  my-3">Your Job </h2>
       </section>
     </div>
-  );
-};
-
+  )
+}
 export default MyJobs;
