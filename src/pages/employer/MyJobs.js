24cc7ab36695e22520bf8de20a0405e003d78607
sarwar-asik <shasik2021@gmail.com>
2023-04-14 15:23:02 +0600

    apply job

--- src/pages/employer/MyJobs.js
+++ src/pages/employer/MyJobs.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useGetJobsQuery, useJobByIdQuery } from "../../features/job/jobApi";
 import useGetData from "../../hooks/useGetData";
+import Swal from "sweetalert2";
 
 const MyJobs = () => {
   // const data = useGetJobsQuery();
@@ -16,6 +17,9 @@ const MyJobs = () => {
   // },[])
 
   console.table("from useGetQuery", data, "my jobs");
+  const applyJobs = (data) => {
+    Swal.fire(` Updating ${data}`);
+  };
 
   return (
     <div className="relative">
@@ -26,28 +30,33 @@ const MyJobs = () => {
         {data.map((item) => {
           const { _id, name, salary, skills, position, overView } = item;
           return (
-            <div className="shadow-xl bg-slate-200 flex gap-3 lg:w-[80%] mx-auto">
+            <div className="shadow-xl bg-slate-200 block lg:flex gap-3 lg:w-[80%] mx-auto">
               <img
-                className="h-[140px] w-[200] rounded-sm"
+                className="h-[30vh] w-full lg:w-[200] rounded-sm"
                 src="https://img.freepik.com/free-photo/rag-dolls-one-blue_1156-199.jpg?w=996&t=st=1681461928~exp=1681462528~hmac=88675e6769508db06ab6cb8ad746d52aefcdd1081c00d54a3b72e8254e24301c"
                 alt="job"
               />
 
-              <aside  className="px-3 pt-4">
-              <h2 className="text-xl font-bold font-serif">
-                    {name || position}
-                  </h2>
-                  <p>
-                    {/* {overView} */}
-                    consectetur adipisicing elit. Laboriosam possimus dicta
-                    deleniti beatae. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  </p>
+              <aside className="px-3 pt-4">
+                <h2 className="text-xl font-bold font-serif">
+                  {name || position}
+                </h2>
+                <p>
+                  {/* {overView} */}
+                  consectetur adipisicing elit. Laboriosam possimus dicta
+                  deleniti beatae. Lorem ipsum dolor sit amet consectetur,
+                  adipisicing elit.
+                </p>
                 <div className="flex justify-between items-center mt-3">
-                  
                   <h3 className="font-semibold text-blue-600">
                     Salary : $ {salary ? salary : "3500"}
                   </h3>
-                  <button className="bg-[#122D61] p-2 text-white font-bold rounded">Apply</button>
+                  <button
+                    onClick={() => applyJobs(position)}
+                    className="bg-[#122D61] p-2 text-white font-bold rounded"
+                  >
+                    Apply
+                  </button>
                 </div>
               </aside>
             </div>
